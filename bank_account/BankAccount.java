5e7889318843062313810a60e1a54ee72f33e242
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-07-01 14:59:39 +0530

    problem number 3

---
+++ bank_account/BankAccount.java
@@ -0,0 +1,40 @@
+package bank_account;
+public class BankAccount {
+    private final String accountNumber;
+    private final String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid Deposit Amount");
+        } else {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient Funds");
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+}
